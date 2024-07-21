ccc02a3fbf7ce4bba0c3ab1d44fcb783f51fd775
Marcelo <marcelo23.m@gmail.com>
2024-07-21 18:55:02 -0400

    search items

--- travelRecommendation/travel_recommendation.js
+++ travelRecommendation/travel_recommendation.js
@@ -6,4 +6,27 @@ document.querySelector('#clearSearchBtn').addEventListener('click', (e) => {
 });
 
 
-S
+displayResult(items){
+    console.log("Items", items);
+}
+
+function searchResult(search, data){
+    const query = search.toLowerCase().trim();
+    if(query === "beach" || query === "beaches" || query === "beachs"){
+        displayResult(data.beachs);
+    }else if(query === "temple" || query === "temples" || query === "templs"){
+        displayResult(data.temples);
+    }else if(query === "contry" || query === "country" || query === "countrie"|| query === "countries"){
+        displayResult(data.countries);
+    }else {
+        console.log("search", data);
+    }
+
+}
+document.querySelector('#btnSearch').addEventListener('click', () => {
+    const query = searchBox.value;
+    fetch(`travel_recommendation_api.json`)
+        .then(response => response.json())
+        .then(data => searchResult(query, data))
+        .catch(error => console.error('Error:', error));
+});
